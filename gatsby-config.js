3edba50cdcadca020c9844dbbc969c80a6fec4fc
React-Echarts <tejashwini@innoventes.co>
2024-04-23 15:02:17 +0530

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,18 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `My First GatsBysitMy First Gatsby Site`,
+    title: `My First GatsBy Site @innoventes`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 }
